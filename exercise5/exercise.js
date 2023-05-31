aeb1012f77ce81ed738108b92a2e4da4bcaa0463
Gianmarco <gianmarco.pesola@live.it>
2023-05-31 19:12:57 +0200

    end

---
+++ exercise5/exercise.js
@@ -0,0 +1,31 @@
+const isLogged = true;
+
+function exercise(isLogged) {
+  return new Promise((resolve, reject) => {
+    if (isLogged == true) {
+      resolve(Math.random());
+    } else {
+      reject(new Error("Not true!"));
+    }
+  });
+}
+
+function exerciseChaining(exercise) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (exercise > 0.5) {
+        resolve({ name: "John", age: 24 });
+      } else {
+        reject(new Error("Not greater then 0.5"));
+      }
+    });
+  }, 500);
+}
+
+exercise(isLogged)
+  .then(exerciseChaining)
+  .then((value) => console.log("l'utente loggato è:", value))
+  .catch((err) => console.log(err))
+  .finally(() => {
+    console.log("io funziono indipendentemente dal risultato");
+  });
