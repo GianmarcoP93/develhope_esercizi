27f27a9525a2878184fd01557f601abea06bb940
Gianmarco <gianmarco.pesola@live.it>
2023-06-12 12:11:53 +0200

    fix

--- exercise9/exercise.js
+++ exercise9/exercise.js
@@ -4,12 +4,29 @@
 //   age: 25,
 // };
 
-let save = () => {
-  const user = {
-    id: 1,
-    name: "John",
-    age: 25,
-  };
-
-  localStorage.setItem("user", JSON.stringify(user));
+const internalMemory = {
+  save: (key, value) => {
+    if (typeof key !== "string") throw new Error("key must be a valid string");
+    if (value === undefined) throw new Error("value cannot be undefined");
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+  get: (key) => {
+    if (typeof key !== "string") throw new Error("key must be a valid string");
+    return JSON.parse(localStorage.getItem(key));
+  },
+  remove: (key) => {
+    if (typeof key !== "string") throw new Error("key must be a valid string");
+    localStorage.removeItem(key);
+  },
+  clear: () => {
+    localStorage.clear();
+  },
 };
+
+const user = {
+  id: 1,
+  name: "John",
+  age: 25,
+};
+
+internalMemory.save("user", user);
