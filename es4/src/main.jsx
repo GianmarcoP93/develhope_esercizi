4c786ac4ec0964f4cfddfc6b8de12e205592f064
Gianmarco <gianmarco.pesola@live.it>
2023-06-25 17:31:42 +0200

    4

---
+++ es4/src/main.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import { Welcome } from "./Welcome.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Welcome name="Gianmarco" age="29" />
+);
