1b8245b81852c5c5e954bde1833b9ec445b92b9f
Gianmarco <gianmarco.pesola@live.it>
2023-06-25 17:08:41 +0200

    2nd exercise

---
+++ es2/src/main.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+import { Welcome } from "./Welcome";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <Welcome name="Gianmarco" />
+  </>
+);
